848e92574a13b8a472c0e4e56461d4a98f2385f1
Jay Patel <79360653+jay3838@users.noreply.github.com>
2021-09-23 18:48:22 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,14 @@
 import firebase from 'firebase';
 
-
 const firebaseConfig = {
-      apiKey: "AIzaSyBHfbnABbg5YG43j6ZGFyZEqjNx0d1U68Y",
-    authDomain: "fbmessanger-3882f.firebaseapp.com",
-    projectId: "fbmessanger-3882f",
-    storageBucket: "fbmessanger-3882f.appspot.com",
-    messagingSenderId: "186217026527",
-    appId: "1:186217026527:web:e8fbf7b7d0a4c9be48aa6a",
-    measurementId: "G-E3WQ0NLQZR"
-  };
+  apiKey: "AIzaSyBzWPqEy63N9w83a0MVLriU2Iu4sFgZLeM",
+  authDomain: "fbmessage-8f0db.firebaseapp.com",
+  projectId: "fbmessage-8f0db",
+  storageBucket: "fbmessage-8f0db.appspot.com",
+  messagingSenderId: "750107158065",
+  appId: "1:750107158065:web:61c62dd4cab222626b9710",
+  measurementId: "G-636JQ1HRZE"
+};
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
